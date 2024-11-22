0a1ca36f1f3be9a5e14d048d2c3293dfaa7e7e8f
logyball <loganballard@gmail.com>
2024-11-22 15:31:12 -0500

    updating polling loop
    adding venv to gitignore
    
    add example, update readme, update requirements
    
    updating readme

--- README.md
+++ README.md
@@ -1,14 +1,89 @@
+# Description
+
+A physical reminder when a new podcast shows up.  Uses RSS feeds and a Raspberry PI Screen to display the reminders.
+
 ## Requirements
 
 #### Locally
 - `python3`
 
 #### With PiHat
-- [ScrollPhatHD PI HAT](https://shop.pimoroni.com/en-us/products/scroll-phat-hd)
+- Raspberry PI compatible with PI HAT [ScrollPhatHD PI HAT](https://shop.pimoroni.com/en-us/products/scroll-phat-hd).  I went with the Zero W
+- `python3`
+- [ScrollPhatHD PI HAT](https://shop.pimoroni.com/en-us/products/scroll-phat-hd) (obviously)
 - Compatible Raspberry PI with I2C connection enabled, and [PI HAT installed](https://learn.pimoroni.com/article/getting-started-with-scroll-phat-hd)
+- On your Raspberry PI: `sudo apt-get install python3-numpy python3-virtualenv`
+
 
 ## Setup
 
 - `python -m virtualenv venv`
 - `source ./venv/bin/activate`
 - `pip install -r requirements.txt`
+
+### Configuration
+
+Configuration is done in a local `.env`.  Copy the `.env.example` to a file called `.env` in the same directory and configure these variables:
+
+- `PODCAST_RSS_URLS`: Set to a comma-delimited list of podcast RSS feeds
+- `ENV`: set to 'local' to work on your machine, set to 'SCROLLPHATHD' to display on your PI
+- `LOOKBACK_HOURS`: set to the number of hours before now you'd like an episode to be considered "new"
+- `POLL_INTERVAL_MIN`: how often to check for new podcasts in minutes
+- `LOG_LEVEL`: logging level
+
+## Running
+
+```sh
+
+# in your virtual environment, simply run python main.py
+
+> python main.py
+
+2024-11-22 16:09:06.739 | DEBUG    | __main__:load_config:12 - loading config
+2024-11-22 16:09:06.739 | DEBUG    | __main__:load_config:25 - config loaded successfully
+2024-11-22 16:09:06.741 | DEBUG    | __main__:loop:149 - beginning polling loop
+2024-11-22 16:09:06.741 | DEBUG    | __main__:poll:102 - polling
+2024-11-22 16:09:06.741 | DEBUG    | __main__:fetch_one:91 - fetching https://citationsneeded.libsyn.com/rss
+2024-11-22 16:09:06.741 | DEBUG    | __main__:parse_feed:59 - parsing feed https://citationsneeded.libsyn.com/rss
+2024-11-22 16:09:07.371 | DEBUG    | __main__:parse_ep:36 - parsing episode
+2024-11-22 16:09:07.372 | DEBUG    | __main__:parse_ep:50 - episode successfully parsed: (Episode 212: Gaza and the Political Utility of Selective Empathy)
+2024-11-22 16:09:07.372 | DEBUG    | __main__:parse_feed:82 - feed successfully parsed https://citationsneeded.libsyn.com/rss
+2024-11-22 16:09:07.373 | DEBUG    | __main__:fetch_one:97 - successfully fetched https://citationsneeded.libsyn.com/rss
+2024-11-22 16:09:07.373 | DEBUG    | __main__:fetch_one:91 - fetching https://podcast.darknetdiaries.com/
+2024-11-22 16:09:07.373 | DEBUG    | __main__:parse_feed:59 - parsing feed https://podcast.darknetdiaries.com/
+2024-11-22 16:09:07.713 | DEBUG    | __main__:parse_ep:36 - parsing episode
+2024-11-22 16:09:07.713 | DEBUG    | __main__:parse_ep:50 - episode successfully parsed: (151: Chris Rock)
+2024-11-22 16:09:07.714 | DEBUG    | __main__:parse_feed:82 - feed successfully parsed https://podcast.darknetdiaries.com/
+2024-11-22 16:09:07.714 | DEBUG    | __main__:fetch_one:97 - successfully fetched https://podcast.darknetdiaries.com/
+2024-11-22 16:09:07.714 | DEBUG    | __main__:fetch_one:91 - fetching https://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss
+2024-11-22 16:09:07.714 | DEBUG    | __main__:parse_feed:59 - parsing feed https://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss
+2024-11-22 16:09:09.137 | DEBUG    | __main__:parse_ep:36 - parsing episode
+2024-11-22 16:09:09.137 | DEBUG    | __main__:parse_ep:50 - episode successfully parsed: (886 - Cabinet Curiosity feat. Alex Nichols (11/18/24))
+2024-11-22 16:09:09.138 | DEBUG    | __main__:parse_feed:82 - feed successfully parsed https://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss
+2024-11-22 16:09:09.138 | DEBUG    | __main__:fetch_one:97 - successfully fetched https://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss
+2024-11-22 16:09:09.138 | DEBUG    | __main__:poll:114 - finished polling
+2024-11-22 16:09:09.138 | DEBUG    | __main__:display_new_podcast_info:119 - displaying new podcast info
+2024-11-22 16:09:09.138 | INFO     | __main__:display_new_podcast_info:126 - New episode of Citations Needed found
+2024-11-22 16:09:09.138 | INFO     | __main__:display_new_podcast_info:127 - Episode published Wed, 20 Nov 2024
+2024-11-22 16:09:09.138 | INFO     | __main__:display_new_podcast_info:128 - Episode title: Episode 212: Gaza and the Political Utility of Selective Empathy
+2024-11-22 16:09:09.138 | INFO     | __main__:display_new_podcast_info:129 - Episode description: "Salvadoran Ties Bloodshed To a 'Culture of Violence'", reported The New York Times in 1981. "The violence in Lebanon is casual, random, and probably addicting," stated the Honolulu Star-Advertiser in 1985. "Muslim life is cheap, most...
+2024-11-22 16:09:09.139 | DEBUG    | __main__:display_new_podcast_info:119 - displaying new podcast info
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:126 - New episode of Darknet Diaries found
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:127 - Episode published Tue, 05 Nov 2024
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:128 - Episode title: 151: Chris Rock
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:129 - Episode description: Chris Rock
+2024-11-22 16:09:09.139 | DEBUG    | __main__:display_new_podcast_info:119 - displaying new podcast info
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:126 - New episode of Chapo Trap House found
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:127 - Episode published Tue, 19 Nov 2024
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:128 - Episode title: 886 - Cabinet Curiosity feat. Alex Nichols (11/18/24)
+2024-11-22 16:09:09.139 | INFO     | __main__:display_new_podcast_info:129 - Episode description: We review the various freaks, toadies, goons, and…
+2024-11-22 16:09:09.139 | DEBUG    | __main__:loop:159 - finished displaying, sleeping for 1 minutes...
+```
+
+## How do I find a Podcast's RSS feed?
+
+I used https://castos.com/tools/find-podcast-rss-feed/
+
+## Future Work
+
+So far I've tested this with `libsyn.com`, `soundcloud.com`, and other podcasts' custom RSS feeds.  Hopefully it's generic enough for all the possible permutations!
--- main.py
+++ main.py
@@ -1,41 +1,43 @@
-import scrollphathd as sphd, feedparser as fp, requests, time, datetime
+import scrollphathd as sphd, feedparser as fp, requests, time, datetime, sys, pytz
 from dotenv import load_dotenv
 from loguru import logger
 from os import environ
 
 DT_FORMAT_FEED = "%a, %d %b %Y %H:%M:%S %Z"
 DT_FORMAT_EP = "%a, %d %b %Y %H:%M:%S %z"
-load_dotenv()
-env_vars = dict(environ)
-podcast_urls = dict.get(env_vars, 'PODCAST_RSS_URLS', []).split(',')
+DT_FORMAT_EP_PRINT = "%a, %d %b %Y"
 
-def parse_feed(url: str) -> tuple:
-    podcast_feed_rss = fp.parse(url)
-    updated_time_raw = podcast_feed_rss['updated'] # TODO add err check
-   
-    try:
-        updated_time_dt = datetime.datetime.strptime(updated_time_raw, DT_FORMAT_FEED)
-    except ValueError as e:
-        logger.error(f"could not parse datetime: {e}")
-        return (None, None, True)
+
+def load_config() -> tuple:
+    logger.debug("loading config")
+
+    env_vars = dict(environ)
+    podcast_urls = env_vars.get('PODCAST_RSS_URLS', None)
+    if not podcast_urls:
+        logger.error('could not find podcast urls to parse')
+        return {}, True
     
-    podcasts = dict.get(podcast_feed_rss, 'entries', [])
-    if len(podcasts) > 0:
-        most_recent_ep = podcasts[0]
-    else:
-        most_recent_ep = None
-    
-    if not most_recent_ep:
-        logger.error('no recent episode found')
-        return (None, None, True)
-    
-    return (updated_time_dt, most_recent_ep, False)
+    env = env_vars.get('ENV', 'local')
+    log_level = env_vars.get('LOG_LEVEL', 'INFO')
+    lookback_hours = int(env_vars.get('LOOKBACK_HOURS', 1)) # TODO add non-int check
+    poll_interval = int(env_vars.get('POLL_INTERVAL_MIN', 30)) # TODO add non-int check
+
+    logger.debug("config loaded successfully")
+    return {
+        'urls': podcast_urls.split(','),
+        'env': env,
+        'log_level': log_level,
+        'lookback': lookback_hours,
+        'interval': poll_interval,
+    }, False
 
 
 def parse_ep(ep: dict) -> tuple:
-    title = dict.get(ep, 'title', 'title not found')
-    subtitle = dict.get(ep, 'subtitle', 'subtitle not found')
-    published_raw = dict.get(ep, 'published', None)
+    logger.debug("parsing episode")
+    
+    title = ep.get('title', 'title not found')
+    subtitle = ep.get('subtitle', 'subtitle not found')
+    published_raw = ep.get('published', None)
     if not published_raw:
         logger.error('no published datetime found for episode')
         return (None, True)
@@ -45,25 +47,130 @@ def parse_ep(ep: dict) -> tuple:
         logger.error(f"could not parse datetime: {e}")
         return (None, True)
 
+    logger.debug(f"episode successfully parsed: ({title})")
     return ({
         'title': title,
         'subtitle': subtitle,
         'published': published_dt
     }, False)
 
-cur_time = datetime.datetime.now()
-for u in podcast_urls:
-    update_time_dt, most_recent_ep, err = parse_feed(url=u)
-    if err:
-        break
+
+def parse_feed(url: str) -> tuple:
+    logger.debug(f"parsing feed {url}")
+
+    podcast_feed_rss = fp.parse(url)
+    
+    podcast_name = podcast_feed_rss.get('feed', {}).get('title', None)
+    if not podcast_name:
+        logger.error('podcast name not found')
+        return {}, True
+    
+    podcasts = podcast_feed_rss.get('entries', [])
+    if len(podcasts) > 0:
+        most_recent_ep = podcasts[0]
+    else:
+        most_recent_ep = None
+    
+    if not most_recent_ep:
+        logger.error('no recent episode found')
+        return {}, True
     
     ep_details, err = parse_ep(ep=most_recent_ep)
     if err:
-        break
+        return {}, True
 
-    if cur_time - datetime.timedelta(hours=600) < update_time_dt:
-        logger.info(f'new episode found: {ep_details["title"]}')
+    logger.debug(f"feed successfully parsed {url}")
+    return {
+        'name': podcast_name,
+        'updated': ep_details.get('published', None),
+        'ep': ep_details,
+    }, False
+
+
+def fetch_one(feed_url: str) -> tuple:
+    logger.debug(f"fetching {feed_url}")
+
+    podcast_info, err = parse_feed(url=feed_url)
+    if err:
+        return {}, True
+
+    logger.debug(f"successfully fetched {feed_url}")
+    return podcast_info, False
+
+
+def poll(cfg: dict) -> dict:
+    logger.debug("polling")
+    
+    podcasts_info = {}
+    podcast_urls = cfg.get('urls')
+    for u in podcast_urls:
+        show_details, err = fetch_one(feed_url=u)
+        if err:
+            continue
+
+        podcast_name = show_details.get('name', 'name not found')
+        podcasts_info[podcast_name] = show_details
+    
+    logger.debug("finished polling")
+    return podcasts_info
+
+
+def display_new_podcast_info(name: str, ep_info: dict, cfg: dict):
+    logger.debug("displaying new podcast info")
+    env = cfg.get('env')
+
+    ep_publish_dt_formatted = datetime.datetime.strftime(ep_info.get('published'), DT_FORMAT_EP_PRINT)
+    ep_title = ep_info.get('title')
+    ep_subtitle = ep_info.get('subtitle')
+
+    logger.info(f"New episode of {name} found")
+    logger.info(f"Episode published {ep_publish_dt_formatted}")
+    logger.info(f"Episode title: {ep_title}")
+    logger.info(f"Episode description: {ep_subtitle}")
+
+    if env == 'SCROLLPHATHD':
+        sphd.clear()
+        sphd.set_brightness(0.2)
+        
+        display_str = f"New episode of {name} published {ep_publish_dt_formatted}. Title: {ep_title}"
+        l = sphd.write_string(display_str)
+        for _ in range(l):
+            sphd.show()
+            sphd.scroll(1)
+            time.sleep(0.005)
+        
+        sphd.clear()
+        sphd.show()
+
+        time.sleep(10)
+
+
+def loop(cfg: dict):
+    logger.debug('beginning polling loop')
+    while True:
+        cur_time = datetime.datetime.now().replace(tzinfo=pytz.UTC)
+        podcasts_info = poll(cfg=cfg)
+
+        for name, info in podcasts_info.items():
+            updated_time = info.get('updated', None)
+            if cur_time - datetime.timedelta(hours=cfg['lookback']) < updated_time:
+                display_new_podcast_info(name=name, ep_info=info.get('ep'), cfg=cfg)
+
+        logger.debug(f"finished displaying, sleeping for {cfg.get('interval')} minutes...")
+        time.sleep(cfg.get("interval") * 60)
+    
+
+def main():
+    load_dotenv(override=True)
+    config, err = load_config()
+    if err or not config:
+        return
+    
+    logger.remove()
+    logger.add(sys.stderr, level=config.get('log_level'))
+
+    loop(cfg=config)
 
 
 if __name__ == '__main__':
-    pass
+    main()
